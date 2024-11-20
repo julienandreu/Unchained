1a5388be2f6fa2b1bdd2ccfe0dae29ffb9c67ad6
Julien Andreu <julienandreu@me.com>
2024-11-20 14:55:50 -0500

    chore: Clean-up deps and starting codebase

--- src/rete/index.ts
+++ src/rete/index.ts
@@ -1,32 +1,94 @@
-/* eslint-disable */
-import { createEditor as createDefaultEditor } from './default'
-import { createEditor as createPerfEditor } from './perf'
-import { createEditor as createCustomEditor } from './customization'
-import { createEditor as create3DEditor } from './3d'
-import { createEditor as createScopesEditor } from './scopes'
-import { isHeadless } from '../headless'
+import { NodeEditor, GetSchemes, ClassicPreset } from 'rete';
 
-const factory = {
-  'default': createDefaultEditor,
-  'perf': createPerfEditor,
-  'customization': createCustomEditor,
-  '3d': create3DEditor,
-  'scopes': createScopesEditor
+import { AreaPlugin, AreaExtensions } from 'rete-area-plugin';
+import {
+  ConnectionPlugin,
+  Presets as ConnectionPresets,
+} from 'rete-connection-plugin';
+import {
+  ReactPlugin,
+  ReactArea2D,
+  Presets as ReactPresets,
+} from 'rete-react-plugin';
+import { createRoot } from 'react-dom/client';
+
+import { CustomNode } from '../customization/CustomNode';
+import { StyledNode } from '../customization/StyledNode';
+import { CustomSocket } from '../customization/CustomSocket';
+import { CustomConnection } from '../customization/CustomConnection';
+
+import { addCustomBackground } from '../customization/custom-background';
+
+type Schemes = GetSchemes<
+  ClassicPreset.Node,
+  ClassicPreset.Connection<ClassicPreset.Node, ClassicPreset.Node>
+>;
+type AreaExtra = ReactArea2D<Schemes>;
+
+const socket = new ClassicPreset.Socket('socket');
+
+export async function createEditor(container: HTMLElement) {
+  const editor = new NodeEditor<Schemes>();
+  const area = new AreaPlugin<Schemes, AreaExtra>(container);
+  const connection = new ConnectionPlugin<Schemes, AreaExtra>();
+  const reactRender = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
+
+  AreaExtensions.selectableNodes(area, AreaExtensions.selector(), {
+    accumulating: AreaExtensions.accumulateOnCtrl()
+  });
+  AreaExtensions.snapGrid(area, {
+    size: 16
+  });
+
+  reactRender.addPreset(
+    ReactPresets.classic.setup({
+      customize: {
+        node(context) {
+          if (context.payload.label === 'Fully customized') {
+            return CustomNode;
+          }
+          if (context.payload.label === 'Override styles') {
+            return StyledNode;
+          }
+          return ReactPresets.classic.Node;
+        },
+        socket() {
+          return CustomSocket;
+        },
+        connection() {
+          return CustomConnection;
+        },
+      },
+    })
+  );
+
+  connection.addPreset(ConnectionPresets.classic.setup());
+
+  addCustomBackground(area);
+
+  editor.use(area);
+  area.use(connection);
+  area.use(reactRender);
+
+  const aLabel = 'Override styles';
+  const bLabel = 'Fully customized';
+
+  const a = new ClassicPreset.Node(aLabel);
+  a.addOutput('a', new ClassicPreset.Output(socket));
+  a.addInput('a', new ClassicPreset.Input(socket));
+  await editor.addNode(a);
+
+  const b = new ClassicPreset.Node(bLabel);
+  b.addOutput('a', new ClassicPreset.Output(socket));
+  b.addInput('a', new ClassicPreset.Input(socket));
+  await editor.addNode(b);
+
+  await area.translate(a.id, { x: 0, y: 0 });
+  await area.translate(b.id, { x: 300, y: 0 });
+
+  await editor.addConnection(new ClassicPreset.Connection(a, 'a', b, 'a'));
+
+  return {
+    destroy: () => area.destroy(),
+  };
 }
-
-const query = typeof location !== 'undefined' && new URLSearchParams(location.search)
-const name = ((query && query.get('template')) || 'default') as keyof typeof factory
-
-const create = factory[name]
-
-if (!create) {
-  throw new Error(`template with name ${name} not found`)
-}
-
-export const createEditor = ((...args: Parameters<typeof create>) => {
-  if (isHeadless()) {
-    args[0].classList.add('headless')
-    document.body.style.overflow = 'hidden';
-  }
-  return create.apply(this, args)
-}) as typeof create
